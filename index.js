d282252544cd88bff825945fc8e482ba386c9cc3
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-06-29 17:48:33 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+let currentUser = "Ashley";
+
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+let firstInitial = currentUser[0];
+let restOfName = currentUser.slice(1);
+
+let shortGreeting =  `Welcome, ${currentUser[0]}!`;
